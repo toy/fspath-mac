3cc5daab800227c37a3e68e76e2f1a631862d6da
Ivan Kuchin <tadump+git@gmail.com>
2014-09-28 20:12:12 +0200

    simplify going to project root in spec_helper for auto compiling ext

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-Dir.chdir File.join(File.dirname(__FILE__), '..') do
+Dir.chdir File.expand_path('../..', __FILE__) do # chdir to project root
   Dir['ext/**/extconf.rb'].each do |extconf|
     Dir.chdir(File.dirname(extconf)) do
       system('ruby extconf.rb') && system('make') or abort "failed compiling #{extconf}"
