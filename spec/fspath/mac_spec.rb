73ae1bee6de3d80aaf32bc5d513243cc8bc25584
Ivan Kuchin <tadump+git@gmail.com>
2014-09-28 21:48:41 +0200

    convert specs to new style

--- spec/fspath/mac_spec.rb
+++ spec/fspath/mac_spec.rb
@@ -10,10 +10,10 @@ describe FSPath::Mac do
         @link.unlink
         @link.make_link(@path)
 
-        @path.move_to_trash.should be_true
+        expect(@path.move_to_trash).to be_truthy
 
-        @path.should_not exist
-        @link.stat.nlink.should == 2
+        expect(@path).not_to exist
+        expect(@link.stat.nlink).to eq(2)
       end
     end
 
@@ -22,7 +22,7 @@ describe FSPath::Mac do
         it "should call finder_label_number" do
           @path = FSPath('to_label')
 
-          @path.should_receive(:finder_label_number).and_return(0)
+          expect(@path).to receive(:finder_label_number).and_return(0)
 
           @path.finder_label
         end
@@ -31,8 +31,8 @@ describe FSPath::Mac do
           @path = FSPath('to_label')
 
           FSPath::FINDER_LABEL_COLORS.each_with_index do |label, index|
-            @path.stub(:finder_label_number).and_return(index)
-            @path.finder_label.should == label
+            allow(@path).to receive(:finder_label_number).and_return(index)
+            expect(@path.finder_label).to eq(label)
           end
         end
       end
@@ -41,7 +41,7 @@ describe FSPath::Mac do
         it "should call finder_label_number=" do
           @path = FSPath('to_label')
 
-          @path.should_receive(:finder_label_number=)
+          expect(@path).to receive(:finder_label_number=)
 
           @path.finder_label = nil
         end
@@ -53,7 +53,7 @@ describe FSPath::Mac do
 
           it "should call label_index.set with apporitate index" do
             FSPath::FINDER_LABEL_COLORS.each_with_index do |label, index|
-              @path.should_receive(:finder_label_number=).with(index).ordered
+              expect(@path).to receive(:finder_label_number=).with(index).ordered
               @path.finder_label = label
             end
           end
@@ -61,21 +61,21 @@ describe FSPath::Mac do
           it "should accept aliases" do
             FSPath::FINDER_LABEL_COLOR_ALIASES.each do |label_alias, label|
               index = FSPath::FINDER_LABEL_COLORS.index(label)
-              @path.should_receive(:finder_label_number=).with(index).ordered
+              expect(@path).to receive(:finder_label_number=).with(index).ordered
               @path.finder_label = label_alias
             end
           end
 
           it "should set to none when called with nil or false" do
-            @path.should_receive(:finder_label_number=).with(0).ordered
+            expect(@path).to receive(:finder_label_number=).with(0).ordered
             @path.finder_label = nil
           end
 
           it "should raise when called with something else" do
             [true, :shitty, 'hello'].each do |label|
-              proc do
+              expect do
                 @path.finder_label = label
-              end.should raise_error("Unknown label #{label.inspect}")
+              end.to raise_error("Unknown label #{label.inspect}")
             end
           end
         end
@@ -98,9 +98,9 @@ describe FSPath::Mac do
           mac_finder_alias_colors = [nil, :orange, :red, :yellow, :blue, :purple, :green, :grey]
           8.times do |label_number|
             @path.send(:finder_label_number=, label_number)
-            @path.send(:finder_label_number).should == label_number
+            expect(@path.send(:finder_label_number)).to eq(label_number)
             color = mac_finder_alias_colors[label_index_through_osascript(@path)]
-            FSPath::Mac::FINDER_LABEL_COLORS.index(color).should == label_number
+            expect(FSPath::Mac::FINDER_LABEL_COLORS.index(color)).to eq(label_number)
           end
         end
       end
@@ -111,9 +111,9 @@ describe FSPath::Mac do
         it "should call comment get using with_argv_tell_finder_to" do
           @path = FSPath(__FILE__)
 
-          @path.should_receive(:with_argv_tell_finder_to).with('get comment of (POSIX file (item 1 of argv) as alias)')
+          expect(@path).to receive(:with_argv_tell_finder_to).with('get comment of (POSIX file (item 1 of argv) as alias)')
 
-          @path.spotlight_comment.should == @comment_text
+          expect(@path.spotlight_comment).to eq(@comment_text)
         end
       end
 
@@ -121,7 +121,7 @@ describe FSPath::Mac do
         it "should call comment set using with_argv_tell_finder_to" do
           @path = FSPath(__FILE__)
 
-          @path.should_receive(:with_argv_tell_finder_to).with('set comment of (POSIX file (item 1 of argv) as alias) to (item 2 of argv)', 'abc')
+          expect(@path).to receive(:with_argv_tell_finder_to).with('set comment of (POSIX file (item 1 of argv) as alias) to (item 2 of argv)', 'abc')
 
           @path.spotlight_comment = 'abc'
         end
@@ -131,19 +131,19 @@ describe FSPath::Mac do
         it "should call comment get using with_argv_tell_finder_to" do
           @path = FSPath.temp_file_path
 
-          @path.spotlight_comment.should == ''
+          expect(@path.spotlight_comment).to eq('')
 
           @path.spotlight_comment = 'abc'
-          @path.spotlight_comment.should == 'abc'
+          expect(@path.spotlight_comment).to eq('abc')
 
           @path.spotlight_comment = 1
-          @path.spotlight_comment.should == '1'
+          expect(@path.spotlight_comment).to eq('1')
 
           @path.spotlight_comment = "def\nghi"
-          @path.spotlight_comment.should == "def\nghi"
+          expect(@path.spotlight_comment).to eq("def\nghi")
 
           @path.spotlight_comment = nil
-          @path.spotlight_comment.should == ''
+          expect(@path.spotlight_comment).to eq('')
         end
       end
     end
