8955f35590dbde50f0820fb630f9c121d7bf765f
Ivan Kuchin <tadump+git@gmail.com>
2014-11-10 16:13:48 +0100

    fix generated Makefile for ruby1.8

--- ext/fspath/mac/extconf.rb
+++ ext/fspath/mac/extconf.rb
@@ -3,3 +3,11 @@ require 'mkmf'
 with_ldflags($LDFLAGS + ' -framework AppKit'){ true }
 
 create_makefile('fspath/mac/ext')
+
+# fix Makefile not containing rule for .m
+makefile = File.read('Makefile')
+if makefile && makefile['.c.o:'] && !makefile['.m.o:']
+  File.open('Makefile', 'w') do |f|
+    f << makefile.sub('.c.o:', '.m.o:')
+  end
+end
